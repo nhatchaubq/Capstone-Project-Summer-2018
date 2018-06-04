0e6538359651dd5737fbea939179d1829f84e770
higo <nhatchau.bq249@gmail.com>
2018-06-05 04:21:21 +0700

    data for db

---
+++ sql/Department.sql
@@ -0,0 +1,3 @@
+INSERT INTO [Department]([Id], [Name]) VALUES (1, N'Administrator'); GO
+INSERT INTO [Department]([Id], [Name]) VALUES (2, N'Construction'); GO
+INSERT INTO [Department]([Id], [Name]) VALUES (3, N'Office'); GO
---
+++ sql/Role.sql
@@ -0,0 +1,5 @@
+INSERT INTO [Role]([Id], [Name]) VALUES (1, N'Admin'); GO
+INSERT INTO [Role]([Id], [Name]) VALUES (2, N'Manager'); GO
+INSERT INTO [Role]([Id], [Name]) VALUES (3, N'Site Manager'); GO
+INSERT INTO [Role]([Id], [Name]) VALUES (4, N'Equipment Staff'); GO
+INSERT INTO [Role]([Id], [Name]) VALUES (5, N'Staff'); GO
