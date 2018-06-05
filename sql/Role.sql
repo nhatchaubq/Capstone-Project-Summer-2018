4ff2b6b03e4b0453bc0098aab4de3b7d6d379920
DESKTOP-9V9UFKT\TienNT <tiennt1005@gmail.com>
2018-06-05 13:07:31 +0700

    update sql

--- sql/Role.sql
+++ sql/Role.sql
@@ -1,5 +1,5 @@
-INSERT INTO [Role]([Id], [Name]) VALUES (1, N'Admin'); GO
-INSERT INTO [Role]([Id], [Name]) VALUES (2, N'Manager'); GO
-INSERT INTO [Role]([Id], [Name]) VALUES (3, N'Site Manager'); GO
-INSERT INTO [Role]([Id], [Name]) VALUES (4, N'Equipment Staff'); GO
-INSERT INTO [Role]([Id], [Name]) VALUES (5, N'Staff'); GO
+INSERT INTO [Role]( [Name]) VALUES ( N'Admin'); 
+INSERT INTO [Role]( [Name]) VALUES ( N'Manager'); 
+INSERT INTO [Role]( [Name]) VALUES ( N'Site Manager'); 
+INSERT INTO [Role]( [Name]) VALUES ( N'Equipment Staff'); 
+INSERT INTO [Role]( [Name]) VALUES ( N'Staff'); 
