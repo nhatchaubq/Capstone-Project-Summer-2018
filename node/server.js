bd98c5e97065eb5b5bbba2fec77f37fa891d768d
Điền <diendtlse61501@fpt.edu.vn>
2018-06-14 11:44:15 +0700

    đổi pass

--- node/server.js
+++ node/server.js
@@ -9,7 +9,7 @@ server.use(cors());
 var connection = {
     server: "localhost",
     userName: "sa",
-    password: "cCS94@bcnq836894",
+    password: "123456",
     port: "1433",
     options: {
         // instanceName : "SQLEXPRESS",
@@ -39,8 +39,8 @@ server.use('/api/Vendor', require('./routes/Vendor'));
 server.use('/api/work_order', require('./routes/work_order'));
 server.use('/api/location', require('./routes/location'));
 server.use('/api/role', require('./routes/accountRole'));
-server.use('/api/team',require('./routes/team'));
-server.use('/api/equipmentItem',require("./routes/equipmentItem"))
+server.use('/api/team', require('./routes/team'));
+server.use('/api/equipmentItem', require("./routes/equipmentItem"))
 server.use('/api/team_location', require('./routes/team_location'));
 
 // catch 404 and forward to error handler
