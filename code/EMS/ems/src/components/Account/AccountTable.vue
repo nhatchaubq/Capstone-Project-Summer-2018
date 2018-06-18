852f548eed7329c33424d7097e417e52fde2face
Điền <diendtlse61501@fpt.edu.vn>
2018-06-18 18:19:54 +0700

    chỉnh table account

--- code/EMS/ems/src/components/Account/AccountTable.vue
+++ code/EMS/ems/src/components/Account/AccountTable.vue
@@ -1,38 +1,71 @@
 <template>
-    <div v-if="accounts">
-        <table class="table">
-             <thead>
-                <tr>
-                    <th><strong>ID</strong></th>
-                    <th><strong>User name</strong></th>
-                    <th><strong>Full name </strong></th>
-                    <th><strong>Email </strong></th>
-                    <th><strong>Phone</strong></th>
-                    <th><strong>Role</strong></th>
-                    <!-- <th><strong>Department</strong></th> -->
-                </tr>
-            </thead>  
-            <tbody>
-                   <tr v-bind:key="account.id" v-for="account in accounts" >
-                    <td>{{account.Id}}</td> 
-
-                      <router-link :to="`/account/${account.Id}`">  <td>{{account.Username  }}</td>  </router-link>
-
-                    <td>{{account.Fullname ? account.Fullname: "N/A" }}</td>
-                    <td>{{account.Email ? account.Email : "N/A" }}</td>
-                    <td>{{account.Phone ? account.Phone : NaN}}</td>
-                    <td>{{account.Role.Name}}</td>
-                    <!-- <td>{{account.Department.Name }}</td> -->
-                </tr>
-
-            </tbody>
-        </table>
-    </div>
+  <div v-if="accounts">
+  
+    <table class="mytable ">
+  
+      <thead>
+  
+        <tr class="txtText">
+  
+          <th><strong>ID</strong></th>
+  
+          <th><strong>User name</strong></th>
+  
+          <th><strong>Full name </strong></th>
+  
+          <th><strong>Email </strong></th>
+  
+          <th><strong>Phone</strong></th>
+  
+          <th><strong>Role</strong></th>
+  
+          <!-- <th><strong>Department</strong></th> -->
+  
+        </tr>
+  
+      </thead>
+  
+      <tbody>
+  
+        <tr v-bind:key="account.id" v-for="account in accounts" class="txtText">
+  
+          <td>{{account.Id}}</td>
+  
+  
+  
+          <router-link :to="`/account/${account.Id}`">
+  
+            <td>{{account.Username }}</td>
+  
+          </router-link>
+  
+  
+  
+          <td>{{account.Fullname ? account.Fullname: "N/A" }}</td>
+  
+          <td>{{account.Email ? account.Email : "N/A" }}</td>
+  
+          <td>{{account.Phone ? account.Phone : NaN}}</td>
+  
+          <td>{{account.Role.Name}}</td>
+  
+          <!-- <td>{{account.Department.Name }}</td> -->
+  
+        </tr>
+  
+  
+  
+      </tbody>
+  
+    </table>
+  
+  </div>
 </template>
 
 <script>
 export default {
   name: "account-table",
+
   props: {
     accounts: null
   }
@@ -42,18 +75,25 @@ export default {
 <style>
 table {
   /* border: 1px solid black; */
+
   width: 100%;
+
   /* text-align: right; */
 }
 
 th {
   text-align: left;
   background-color: #cfd8dc;
+  height: 30px;
+  line-height: 30px;
+  padding: 0.5rem !important;
 }
 
 td {
   vertical-align: middle;
-  /* text-align: justify; */
+  height: 40px;
+  line-height: 25px;
+  padding: 0.5rem;
 }
 
 tr:nth-child(odd) {
@@ -62,12 +102,37 @@ tr:nth-child(odd) {
 }
 
 tr:nth-child(even) {
-  background-color: #eceff1;
+  background-color: #f5f5f5;
   color: #263238;
 }
 
 #th1:curve {
   text-align: justify;
+
   background-color: #b0bec5;
+}
+
+tr:hover {
+  background-color: #eceff1;
+}
+
+.txtText {
+  text-align: left;
+
+  /*margin: 0.3rem;*/
+
+  padding: 0.5rem;
+
+  background-color: #cfd8dc;
+}
+
+table {
+  /* border: 1px solid black; */
+
+  width: 100%;
+
+  font-size: 15px;
+
+  /* text-align: right; */
 }
 </style>
