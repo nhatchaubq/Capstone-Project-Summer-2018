0873507133c7e361cd6ec24a551bfd49d9c4dea0
Điền <diendtlse61501@fpt.edu.vn>
2018-06-22 20:49:33 +0700

    thêm thông tin vendor details

--- code/EMS/ems/src/components/Vendor/VendorDetail.vue
+++ code/EMS/ems/src/components/Vendor/VendorDetail.vue
@@ -1,233 +1,52 @@
 <template>
-  <div v-if="team" class="material-box" style="width: 50%">
+  <div v-if="!team" class="material-box" style="width: 50%"> 
       <div class="row">
-        <h2 class="col-10" style="font-size: 30px; color: #3960A4">{{team.Name}}</h2>
-        <button class="button btn-edit btn-primary material-shadow-animate " v-on:click="$store.state.teamPage.detailPage.editMode = !editMode">Edit</button>
+        <h2 class="col-10" style="font-size: 30px; color: #3960A4">{{vendor.BusinessName}}</h2>
+        <button class="button btn-edit btn-primary material-shadow-animate " v-on:click="$store.state.vendorPage.detailPage.editMode = !editMode">Edit</button>
       </div>
-      <div>
-        <h2>Create date: {{team.CreatedDate}} </h2>
-  
-        <strong >Leader</strong>
-        
-        <div >
-          <!-- <div class="row">
-            <model-select v-if="editMode" class="col-9" style="margin-right:1rem; "  :options="toLeaderOptions" v-model="selectedToLeader"  placeholder="Select a leader"></model-select>  
-              <button v-if="editMode" class="button btn-primary " style="margin-top: 1rem; padding :-1rem" v-on:click="console.error(this.account.id)">add new</button>     
-          </div> -->
-          <div v-if="team.LeaderAccount">
-            <div  class="row " style="height:43.42px">
-              <div class="col-8">
-                <span>-</span>
-              <router-link :to="`/account/${team.LeaderAccount.Id}`">
-                {{team.LeaderAccount.Fullname ? team.LeaderAccount.Fullname :'N/A' }}
-              </router-link>
-                <span>({{team.LeaderAccount.Username}})</span>
-              </div>
-            </div>
-          </div>
-        </div>  
-        <div v-if="team.MemberAccounts">
-            <strong>Members</strong>
-            <div :key="member.Id" v-for="member in team.MemberAccounts">
-              <div class="row" style="height:40px; ">
-                <div class="col-8">
-                    <span>-</span>
-                    <router-link :to="`/account/${member.Id}`">
-                      {{member.Fullname ? member.Fullname :'N/A' }}
-                    </router-link>
-                      <span>({{member.Username}})</span>
-                </div >
-                <div class="col-3" >
-                  <button v-if="editMode" class="button btn-edit btn-primary material-shadow-animate "   v-on:click="changeToLeader(member.Id)">Set to leader</button>
-                </div>
-                <div class="col-1">
-                  <button v-if="editMode" class="material-icons"  style="color: var(--danger); text-align: center; padding-bottom: 3px; margin-top: 4px" v-on:click="kick(member.Id)">close</button>
-                </div>
-              </div>
-            </div>
-        </div>
-      </div>
-    <strong v-if="editMode">Add new members: </strong>
-    <div class="field is-horizontal">
-      <multi-select  v-if="editMode" style="width: 100% !important; height:36px; margin-right: 1rem"  :options="memberOptions" :selected-options="selectedMemberList" @select="onSelect" placeholder="Select a member"></multi-select> 
-        <button v-if="editMode" class="button btn-primary material-shadow-animate pull-right" style="margin-top: 4px" v-on:click="addNew()">add new</button> 
 
+    <div class="row">
+      <div class="col-4" >Business address</div>
+      <div class="col-7"> {{vendor.BusinessAddress}} </div>
+    </div>
+    <div class="row">
+      <div class="col-4" >Website</div>
+      <div class="col-7"> {{vendor.Website}} </div>
+    </div>
+    <div class="row">
+      <div class="col-4" >Contact name</div>
+      <div class="col-7"> {{vendor.ContactName}} </div>
+    </div>
+    <div class="row">
+      <div class="col-4" >Contact email</div>
+      <div class="col-7"> {{vendor.ContactEmail}} </div>
+    </div>
+    <div class="row">
+      <div class="col-4" >Description</div>
+      <div class="col-7"> {{vendor.Description}} </div>
     </div>
 
+
+
+
+
   </div>
-         
-
-
 </template>
 
 <script>
-import { sync } from "vuex-pathify";
-import VueBase64FileUpload from "vue-base64-file-upload";
-import { BasicSelect, MultiSelect, ModelSelect } from "vue-search-select";
 export default {
-  components: {
-    VueBase64FileUpload,
-    MultiSelect,
-    BasicSelect,
-    ModelSelect
-  },
   created() {
-    let teamApiUrl = `http://localhost:3000/api/team/id/${
-      this.$route.params.id
-    }`;
-
-    this.axios.get(teamApiUrl).then(response => {
-      let data = response.data.Team;
-      this.team = data;
-
-      // data.forEach(member => {
-      //     let option = {
-      //       text: member.Name,
-      //       value: member.Id
-      //     };
-      //     this.memberOptions.push(option);
-      //   });
+    let URL = `http://localhost:3000/api/vendor/${this.$route.params.id}`;
+    this.axios.get(URL).then(response => {
+      let data = response.data;
+      this.vendor = data;
     });
-
-    let outsideTeamApiUrl = `http://localhost:3000/api/AllAccExceptThatTeam/${
-      this.$route.params.id
-    }`;
-    this.axios.get(outsideTeamApiUrl).then(res => {
-      let data = res.data;
-      data.forEach(element => {
-        let option = {
-          value: element.Id,
-          text: element.Fullname
-        };
-        this.memberOptions.push(option);
-      });
-    });
-    // let getMembersInTeamApiUrl = `http://localhost:3000/api/getMembersInTeam/${
-    //   this.$route.params.id
-    // }`;
-    // this.axios.get(getMembersInTeamApiUrl).then(res => {
-    //   let data = res.data;
-    //   data.forEach(element => {
-    //     let option = {
-    //       value: element.account.Id,
-    //       text: element.account.Fullname
-    //     };
-    //     this.toLeaderOptions.push(option);
-    //   });
-    // });
-    this.axios
-      .get(
-        `http://localhost:3000/api/team/id/getMembersInTeam/${
-          this.$route.params.id
-        }`
-      )
-      .then(response => {
-        let data = response.data;
-        data.forEach(element => {
-          let option = {
-            value: element.Id,
-            text: element.Fullname
-          };
-          this.toLeaderOptions.push(option);
-          console.log(this.toLeaderOptions.length);
-        });
-      })
-      .catch(error => {
-        alert(error);
-      });
   },
-
   data() {
     return {
-      team: null,
-      memberOptions: [],
-      toLeaderOptions: [],
-      selectedMember: {
-        value: "",
-        text: ""
-      },
-      selectedMemberList: [],
-      lastSelectItem: {}
-      // selectedToLeader: {
-      //   value: "",
-      //   text: ""
-      // }
+      vendor: null,
+      checkedActive: []
     };
-  },
-  computed: {
-    editMode: sync("teamPage.detailPage.editMode")
-  },
-  methods: {
-    createAccount1() {
-      this.axios
-        .post(
-          `http://localhost:3000/api/AllAccExceptThatTeam/${
-            this.$route.params.id
-          }`,
-          {
-            account: this.account
-          }
-        )
-        .then(res => {
-          this.$router.push("/team/:id");
-        });
-    },
-    onSelect(items, lastSelectItem) {
-      this.selectedMemberList = items;
-      this.lastSelectItem = lastSelectItem;
-    },
-    onSelect1(item) {
-      this.item = item;
-    },
-    addNew() {
-      this.selectedMemberList.forEach(element => {
-        this.axios
-          .post(
-            `http://localhost:3000/api/AllAccExceptThatTeam/${
-              this.$route.params.id
-            }`,
-            {
-              accountID: element.value
-            }
-          )
-          .then(res => {
-            location.reload();
-          });
-      });
-    },
-    kick(accountID) {
-      this.axios
-        .delete(
-          `http://localhost:3000/api/team/id/${
-            this.$route.params.id
-          }/${accountID}`
-        )
-        .then(res => {
-          location.reload();
-        });
-    },
-    changeToLeader(memberID) {
-      let leaderId = null;
-      if (this.team.LeaderAccount) {
-        leaderId = this.team.LeaderAccount.Id;
-      }
-      this.axios
-        .put(
-          `http://localhost:3000/api/team/id/${
-            this.$route.params.id
-          }/${memberID}/${leaderId}`
-        )
-        .then(res => {
-          location.reload();
-        });
-    }
-    // changeNewLeader(leaderId, memberID) {
-    //   this.axios.push(
-    //     `http://localhost:3000/api/team/id/${
-    //       this.$route.params.id
-    //     }/${memberID}/${leaderId}`
-    //   );
-    // }
   }
 };
 </script>
