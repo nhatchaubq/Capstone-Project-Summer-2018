41529fd9433a7f6bb695088b28db8c00d6838967
Điền <diendtlse61501@fpt.edu.vn>
2018-06-26 17:05:41 +0700

    chỉnh lại dashboard

--- node/routes/dashBoard.js
+++ node/routes/dashBoard.js
@@ -8,7 +8,15 @@ router.get("/", (request, response) => {
             "(SELECT COUNT(Id)   FROM [WorkOrder]   WHERE StatusID =4 ) as 'Dashboard.InProgress'," +
             "(SELECT COUNT(Id)   FROM [WorkOrder]   WHERE StatusID =5 ) as 'Dashboard.Closed'," +
             "(SELECT COUNT(Id)   FROM [WorkOrder]   WHERE StatusID =6 ) as 'Dashboard.Rejected'," +
-            "(SELECT COUNT(Id)   FROM [WorkOrder]   WHERE StatusID =1009 ) as 'Dashboard.Cancelled'" +
+            "(SELECT COUNT(Id)   FROM [WorkOrder]   WHERE StatusID =1009 ) as 'Dashboard.Cancelled'," +
+            "(SELECT Count(Id) FROM [WorkOrder] WHERE CategoryID = 1 and convert(date, getdate()) = convert(date, DueDate)) as 'Dashboard.MaintainCountToday'," +
+            "(SELECT Count(Id) FROM [WorkOrder] WHERE CategoryID = 1 and convert(date, dateadd(day, 1, getdate())) = convert(date, DueDate)) as 'Dashboard.MaintainCountTomorrow'," +
+            "(SELECT Count(Id) FROM [WorkOrder] WHERE CategoryID = 2 and  StatusID =4 and convert(date, getdate()) = convert(date, DueDate) ) as 'Dashboard.ReturnCountToday'," +
+            "(SELECT Count(Id) FROM [WorkOrder] WHERE CategoryID = 2 and  StatusID =4 and convert(date, dateadd(day, 1, getdate())) = convert(date, DueDate) ) as 'Dashboard.ReturnCountTomorrow'," +
+            "(SELECT Count(WorkOrderID) FROM [WorkOrderDetail] WHERE WorkOrderID IN (SELECT Id FROM [WorkOrder] WHERE CategoryID = 1 and convert(date, getdate()) = convert(date, DueDate))) as 'Dashboard.MaintainCountTodayQItem'," +
+            "(SELECT Count(WorkOrderID) FROM [WorkOrderDetail] WHERE WorkOrderID IN (SELECT Id FROM [WorkOrder] WHERE CategoryID = 1 and convert(date, dateadd(day, 1, getdate())) = convert(date, DueDate))) as 'Dashboard.MaintainCountTomorrowQItem'," +
+            "(SELECT Count(WorkOrderID) FROM [WorkOrderDetail] WHERE WorkOrderID IN (SELECT Id FROM [WorkOrder] WHERE CategoryID = 2 and StatusID =4 and convert(date, getdate()) = convert(date, DueDate))) as 'Dashboard.ReturnCountTodayQItem'," +
+            "(SELECT Count(WorkOrderID) FROM [WorkOrderDetail] WHERE WorkOrderID IN (SELECT Id FROM [WorkOrder] WHERE CategoryID = 2 and StatusID =4 and convert(date, dateadd(day, 1, getdate())) = convert(date, DueDate))) as 'Dashboard.ReturnCountTomorrowQItem'" +
             "FOR JSON PATH,  without_array_wrapper")
         .into(response);
 });
