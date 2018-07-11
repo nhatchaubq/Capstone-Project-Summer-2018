9f006e7b6e3e31d77dffe2d764dc55db91ea12c5
Điền <diendtlse61501@fpt.edu.vn>
2018-07-12 06:28:43 +0700

    thêm validate cho account detail

--- code/EMS/ems/src/components/Account/AccountDetail.vue
+++ code/EMS/ems/src/components/Account/AccountDetail.vue
@@ -36,13 +36,13 @@
 </div>
 <div  class="row " style="height: 36px" >
     <div class="col-12" style="margin-top:0.5rem" >
-      Full name:  
-    </div>
+      Full name:  <span v-if="CreateAccountErrors.NoFullname != ''">. <span class="error-text">{{ CreateAccountErrors.NoFullname }}</span></span>
+    </div>  
 </div>
   <input v-if="!editMode" v-model="account.Fullname" class="input col-7 " type="text"  placeholder="Text input" disabled="disabled">
   <input v-else v-model="account.Fullname" class="input col-7 " type="text"  placeholder="Text input" >
 <div v-if="editMode" class="row" style="margin-top:0.5rem;  height: 36px">
-    <div class="col-12" style="margin-top:0.5rem">Password: </div>
+    <div class="col-12" style="margin-top:0.5rem">Password: <span v-if="CreateAccountErrors.NoPassword != ''"> <span class="error-text">{{ CreateAccountErrors.NoPassword }}</span></span><span v-show="CreateAccountErrors.WeakAccount != ''"> <span class="error-text">.{{ CreateAccountErrors.WeakAccount }}</span></span> </div> 
 </div>
     <input v-if="editMode" v-model ="account.Password"  class="input col-7 " type="password"  placeholder="Text input">
 <!-- <div class="row" style="margin-top:0.5rem;  height: 36px">
@@ -80,13 +80,13 @@
   <div class="col-7" style="padding-left: 0 !important;"> {{account.StartDate ? getDate(account.StartDate): 'N/A' }}</div>
 <!-- <h2 style="padding-top:0.9rem;padding-bottom: 0.9rem">Start date: {{account.StartDate ? account.StartDate: 'N/A' }}</h2> -->
 <div class="row" style="margin-top:0.5rem; height: 36px">
-  <div class="col-12" style="margin-top:0.5rem">Email: </div>
+  <div class="col-12" style="margin-top:0.5rem">Email: <span v-if="CreateAccountErrors.NoEmail != ''">. <span class="error-text">{{ CreateAccountErrors.NoEmail }}</span></span> </div> 
   <!-- <div class="col-7">{{account.Email ?account.Email: 'N/A' }} </div> -->
 </div>
   <input v-if="!editMode" v-model="account.Email" class="input col-7 " type="text"  placeholder="Text input" disabled="disabled">
   <input v-else v-model="account.Email" class="input col-7 " type="text"  placeholder="Text input">
 <div class="row" style="margin-top:0.5rem; height: 36px">
-  <div class="col-12" style="margin-top:0.5rem">Phone: </div>
+  <div class="col-12" style="margin-top:0.5rem">Phone: <span v-if="CreateAccountErrors.NoPhone != ''">. <span class="error-text">{{ CreateAccountErrors.NoPhone }}</span></span> </div> 
   <!-- <div class="col-7">{{account.Phone ? account.Phone: 'N/A' }} </div> -->
 </div>
   <input v-if="!editMode" v-model="account.Phone" class="input col-7 " type="text"  placeholder="Text input" disabled="disabled">
@@ -149,6 +149,27 @@ export default {
 
   data() {
     return {
+      sending: false,
+      ErrorStrings: {
+        // NoUsername: 'You must provide username for this account',
+        NoPassword: "You must provide password for this account",
+        NoFullname: "You must provide full name for this account",
+        NoPhone: "You must provide phone number for this account",
+        NoEmail: "You must provide email name for this account",
+        // NoRole: "You must provide role for this account"
+
+        WeakAccount: "Minimum password length is 6 characters"
+      },
+      CreateAccountErrors: {
+        // NoUsername: '',
+        NoPassword: "",
+        NoFullname: "",
+        NoPhone: "",
+        NoEmail: "",
+        // NoRole: ""
+
+        WeakAccount: ""
+      },
       account: null,
       checkedActive: [],
       editMode: false
@@ -156,17 +177,50 @@ export default {
   },
   methods: {
     editAccount() {
-      this.axios
-        .put(`http://localhost:3000/api/account/${this.$route.params.id}`, {
-          account: this.account
-        })
-        .then(res => {
-          // this.$router.push("/account");
-          if (res.status == 200) {
-            this.getAccountDetail(this.$route.params.id);
-            this.editMode = false;
-          }
-        });
+      // if(this.account.username === ''){
+      //     this.CreateAccountErrors.NoUsername = this.ErrorStrings.NoUsername;
+      // }
+      if (this.account.Password === "") {
+        this.CreateAccountErrors.NoPassword = this.ErrorStrings.NoPassword;
+      }
+      if (this.account.Password.length < 6) {
+        this.CreateAccountErrors.WeakAccount = this.ErrorStrings.WeakAccount;
+      }
+      if (this.account.Fullname === "") {
+        this.CreateAccountErrors.NoFullname = this.ErrorStrings.NoFullname;
+      }
+      if (this.account.Phone === "") {
+        this.CreateAccountErrors.NoPhone = this.ErrorStrings.NoPhone;
+      }
+      if (this.account.Email === "") {
+        this.CreateAccountErrors.NoEmail = this.ErrorStrings.NoEmail;
+      }
+      // if (!this.account.roleid || this.account.roleid == "") {
+      //   this.CreateAccountErrors.NoRole = this.ErrorStrings.NoRole;
+      // }
+      if (this.validateAccount())
+        this.axios
+          .put(`http://localhost:3000/api/account/${this.$route.params.id}`, {
+            account: this.account
+          })
+          .then(res => {
+            // this.$router.push("/account");
+            if (res.status == 200) {
+              this.getAccountDetail(this.$route.params.id);
+              this.editMode = false;
+            }
+          });
+    },
+    validateAccount() {
+      return (
+        this.CreateAccountErrors.NoPassword === "" &&
+        this.CreateAccountErrors.NoFullname === "" &&
+        this.CreateAccountErrors.NoPhone === "" &&
+        this.CreateAccountErrors.NoEmail === "" &&
+        //&& this.CreateAccountErrors.NoRole === ""
+        this.CreateAccountErrors.WeakAccount === ""
+        //  && this.CreateAccountErrors.NoUsername === ''
+      );
     },
     getAccountDetail(accountId) {
       let URL = `http://localhost:3000/api/account/id/${accountId}`;
@@ -178,6 +232,41 @@ export default {
     getDate(date) {
       return moment(date).format("L");
     }
+  },
+  watch: {
+    // "account.username": function(){
+    //     if(this.account.username != ''){
+    //         this.CreateAccountErrors.NoUsername = ''
+    //     }
+    // },
+    "account.Password": function() {
+      if (this.account.Password != "") {
+        this.CreateAccountErrors.NoPassword = "";
+      }
+      if (this.account.Password.length > 5) {
+        this.CreateAccountErrors.WeakAccount = "";
+      }
+    },
+    "account.Fullname": function() {
+      if (this.account.Fullname != "") {
+        this.CreateAccountErrors.NoFullname = "";
+      }
+    },
+    "account.Phone": function() {
+      if (this.account.Phone != "") {
+        this.CreateAccountErrors.NoPhone = "";
+      }
+    },
+    "account.Email": function() {
+      if (this.account.Email != "") {
+        this.CreateAccountErrors.NoEmail = "";
+      }
+    }
+    // "account.roleid": function() {
+    //   if (this.account.roleid && this.account.roleid != "") {
+    //     this.CreateAccountErrors.NoRole = "";
+    //   }
+    // }
   }
 };
 </script>
