60df436d3356929109a438786ffd847f52d62574
hautt <hautt1995@gmail.com>
2018-07-17 00:27:33 +0700

    new

--- node/routes/location.js
+++ node/routes/location.js
@@ -4,7 +4,7 @@ var TYPES = require("tedious").TYPES;
 router.get("/", (request, response) => {
   request
     .sql(
-      "Select * From [Location] as l order by l.[Name] desc " + "for json path"
+      "Select * From [Location] as l order by IsActive desc " + "for json path"
     )
     .into(response);
 });
@@ -24,7 +24,7 @@ router.delete("/deleteTeamWithoutWO/:id", (req, res) => {
 router.put("/updateLocation", (req, res) => {
   req
     .sql(
-      "Update [Location] set Name = @name,Address = @address,Description = @description " +
+      "Update [Location] set Name = @name,Address = @address,Description = @description,IsActive = @isActive " +
         "where Id = @locationId"
     )
 
@@ -32,31 +32,35 @@ router.put("/updateLocation", (req, res) => {
     .param("name", req.body.newLocation.name, TYPES.NVarChar)
     .param("address", req.body.newLocation.address, TYPES.NVarChar)
     .param("description", req.body.newLocation.description, TYPES.NVarChar)
+    .param("isActive", req.body.newLocation.isActive, TYPES.Bit)
     .exec(res);
 });
 
-router.put('/update_location_floor_plan/:locationId', (req, res) => {
-  req.sql('update [Location] set [Image] = @imageUrl where Id = @locationId')
-    .param('locationId', req.params.locationId, TYPES.Int)
-    .param('imageUrl', req.body.imageUrl, TYPES.VarChar)
+router.put("/update_location_floor_plan/:locationId", (req, res) => {
+  req
+    .sql("update [Location] set [Image] = @imageUrl where Id = @locationId")
+    .param("locationId", req.params.locationId, TYPES.Int)
+    .param("imageUrl", req.body.imageUrl, TYPES.VarChar)
     .exec(res);
 });
 
 router.get("/floor_block_tile/:locationId", (req, res) => {
   req
-    .sql("select lo.*, (select bl.*, (select fl.*, (select ti.* " +
-      "      from Tile as ti " +
-      "       where ti.FloorID = fl.Id " +
-      "       for json path) as [Tiles] " +
-      " from [Floor] as fl " +
-      " where fl.BlockID = bl.Id " +
-      " for json path) as [Floors] " +
-      " from [Block] as bl " +
-      " where bl.LocationID = @locationId " +
-      " for json path) as [Blocks] " +
-      " from [Location] as lo " +
-      " where lo.Id = @locationId " +
-      " for json path, without_array_wrapper")
+    .sql(
+      "select lo.*, (select bl.*, (select fl.*, (select ti.* " +
+        "      from Tile as ti " +
+        "       where ti.FloorID = fl.Id " +
+        "       for json path) as [Tiles] " +
+        " from [Floor] as fl " +
+        " where fl.BlockID = bl.Id " +
+        " for json path) as [Floors] " +
+        " from [Block] as bl " +
+        " where bl.LocationID = @locationId " +
+        " for json path) as [Blocks] " +
+        " from [Location] as lo " +
+        " where lo.Id = @locationId " +
+        " for json path, without_array_wrapper"
+    )
     .param("locationId", req.params.locationId, TYPES.Int)
     .into(res);
 });
@@ -73,8 +77,8 @@ router.get("/:id", (req, res) => {
 router.post("/create", (request, response) => {
   request
     .sql(
-      "insert into [Location]([Name],[Address],[Description],Longitude,Latitude) " +
-        "values(@name, @address, @description, @longtitude, @latitude) " +
+      "insert into [Location]([Name],[Address],[Description],Longitude,Latitude,IsActive) " +
+        "values(@name, @address, @description, @longtitude, @latitude,@isActive) " +
         "select(select SCOPE_IDENTITY()) as [NewLocationId] for json path, without_array_wrapper"
     )
     .param("name", request.body.newLocation.name, TYPES.NVarChar)
@@ -82,6 +86,7 @@ router.post("/create", (request, response) => {
     .param("description", request.body.newLocation.description, TYPES.NVarChar)
     .param("longtitude", request.body.newLocation.longtitude, TYPES.Float)
     .param("latitude", request.body.newLocation.latitude, TYPES.Float)
+    .param("isActive", request.body.newLocation.isActive, TYPES.Bit)
     .into(response);
 });
 
@@ -89,17 +94,21 @@ router.get("/editLocation/:id", (request, response) => {
   request
     .sql(
       "select l.*,(select tl.Id as 'TeamLocationId',t.Id,t.Name " +
-        "				from TeamLocation as tl join Location as l on tl.LocationID = l.Id " +
-        "											join Team as t on t.Id = tl.TeamID " +
-        "	where tl.LocationID = @locationId for json path) as 'Team', (select COUNT(wo.Id) " +
-        "	from TeamLocation as tl join Location as l on tl.LocationID = l.Id " +
-        "	join WorkOrder as wo on tl.Id = wo.TeamLocationID " +
-        "	where tl.LocationID = @locationId) as 'WorkOrderQuantity', (select distinct tl.Id, tl.TeamID " +
-        "	from TeamLocation as tl join WorkOrder as wo on tl.Id = wo.TeamLocationID " +
-        "	where tl.LocationID = @locationId for json path) as 'TeamWithWorkOrdering' " +
-        "	from[Location] as l " +
-        "	where l.Id = @locationId for json path"
+        " from TeamLocation as tl join Location as l on tl.LocationID = l.Id " +
+        " join Team as t on t.Id = tl.TeamID " +
+        " where tl.LocationID = @locationId for json path) as 'Team', (select distinct tl.Id, tl.TeamID " +
+        " from TeamLocation as tl join WorkOrder as wo on tl.Id = wo.TeamLocationID " +
+        " join WorkOrderStatus as ws on ws.Id = wo.StatusID " +
+        " where tl.LocationID = @locationId and ws.Name != 'Closed' and ws.Name != 'Closed'  for json path) as 'TeamWithWorkOrdering', " +
+        " (select ei.Id " +
+        "   from EquipmentItem as ei join Tile as t on ei.TileID = t.Id " +
+        "   join[Floor] as f on f.Id = t.FloorID " +
+        "   join[Block] as b on f.BlockID = b.Id " +
+        "   where b.LocationID = @locationId	for json path) as 'Items' " +
+        "   from[Location] as l " +
+        " where l.Id = @locationId for json path"
     )
+    // "exec Get1LocationById @locationId"
     .param("locationId", request.params.id, TYPES.Int)
     .into(response);
 });
@@ -108,10 +117,12 @@ router.get("/editLocation/:id", (request, response) => {
 // get team of location for create work order
 router.get("/:id/team/:userId", (req, res) => {
   req
-    .sql("select (json_query((select * from Team where Id = tl.TeamID for json path, without_array_wrapper))) as [Team] "
-    + " from TeamLocation as tl join Team as t on tl.TeamID = t.Id join TeamAccount as ta on t.Id = ta.TeamID "
-    + " where tl.LocationID = @locationId and ta.TeamRoleID = (select Id from TeamRoles where TeamRole = N'Leader') and ta.AccountID = @userId "
-    + " for json path")
+    .sql(
+      "select (json_query((select * from Team where Id = tl.TeamID for json path, without_array_wrapper))) as [Team] " +
+        " from TeamLocation as tl join Team as t on tl.TeamID = t.Id join TeamAccount as ta on t.Id = ta.TeamID " +
+        " where tl.LocationID = @locationId and ta.TeamRoleID = (select Id from TeamRoles where TeamRole = N'Leader') and ta.AccountID = @userId " +
+        " for json path"
+    )
     .param("locationId", req.params.id, TYPES.Int)
     .param("userId", req.params.userId, TYPES.Int)
     .into(res);
--- node/routes/work_order.js
+++ node/routes/work_order.js
@@ -4,20 +4,20 @@ const TYPES = require("tedious").TYPES;
 router.get("/workorderbylocationId/:id", (request, response) => {
   request
     .sql(
-      "select distinct wo.*,t.Name as 'Team' ,p.[Name] as 'Priority',p.TagHexColor,ws.Name as 'Status',wc.Name as 'Category',(select distinct e.Id,e.Name,wd.WorkOrderID,wd.StartDate,wd.FinishedDate,wd.ReturnDate,(select wd.*,ei.SerialNumber,ei.EquipmentID " +
-        "     from WorkOrderDetail as wd join EquipmentItem as ei on wd.EquipmentItemID = ei.Id								 " +
-        " where wd.WorkOrderID = wo.Id and ei.EquipmentID = e.Id for json path) as 'EquipmentItems' " +
-        " from WorkOrderDetail as wd join EquipmentItem as ei on wd.EquipmentItemID = ei.Id " +
-        " join Equipment as e on e.Id = ei.EquipmentID " +
-        " where wd.WorkOrderID = wo.Id  for json path) as 'WorkorderDetail' " +
-        " from WorkOrder as wo join TeamLocation as tl on tl.Id = wo.TeamLocationID " +
-        " join Team as t on t.Id = tl.TeamId " +
-        " join WorkOrderDetail as wd on wo.Id = wd.WorkOrderID " +
-        " join[Priority] as p on p.Id = wo.PriorityID " +
-        " join WorkOrderStatus as ws on ws.Id = wo.StatusID " +
-        " join WorkOrderCategory as wc on wc.Id = wo.CategoryID " +
-        "  where tl.LocationID = @locationId for json path"
-      // "exec [GetWorkOrderByLocationId] @locationId"
+      // "select distinct wo.*,t.Name as 'Team' ,p.[Name] as 'Priority',p.TagHexColor,ws.Name as 'Status',wc.Name as 'Category',(select distinct e.Id,e.Name,wd.WorkOrderID,wd.StartDate,wd.FinishedDate,wd.ReturnDate,(select wd.*,ei.SerialNumber,ei.EquipmentID " +
+      //   "     from WorkOrderDetail as wd join EquipmentItem as ei on wd.EquipmentItemID = ei.Id								 " +
+      //   " where wd.WorkOrderID = wo.Id and ei.EquipmentID = e.Id for json path) as 'EquipmentItems' " +
+      //   " from WorkOrderDetail as wd join EquipmentItem as ei on wd.EquipmentItemID = ei.Id " +
+      //   " join Equipment as e on e.Id = ei.EquipmentID " +
+      //   " where wd.WorkOrderID = wo.Id  for json path) as 'WorkorderDetail' " +
+      //   " from WorkOrder as wo join TeamLocation as tl on tl.Id = wo.TeamLocationID " +
+      //   " join Team as t on t.Id = tl.TeamId " +
+      //   " join WorkOrderDetail as wd on wo.Id = wd.WorkOrderID " +
+      //   " join[Priority] as p on p.Id = wo.PriorityID " +
+      //   " join WorkOrderStatus as ws on ws.Id = wo.StatusID " +
+      //   " join WorkOrderCategory as wc on wc.Id = wo.CategoryID " +
+      //   "  where tl.LocationID = @locationId for json path"
+      "exec [GetWorkOrderByLocationId] @locationId"
     )
     .param("locationId", request.params.id, TYPES.Int)
     .into(response);
