bbc53f6c935147567e6b75c9a5d37abf3d569044
Điền <diendtlse61501@fpt.edu.vn>
2018-07-19 20:44:57 +0700

    team details server

--- node/routes/teamDetails.js
+++ node/routes/teamDetails.js
@@ -4,17 +4,31 @@ var TYPES = require("tedious").TYPES;
 router.get("/:id", (request, response) => {
   request
     .sql(
-      "	 SELECT t.id as 'team.Id', t.Name as 'team.Name', t.CreatedDate as 'team.CreatedDate',  " +
-      "json_query((SELECT a.*,ta.TeamRoleID FROM [TeamAccount] as ta JOIN [Account] as a ON ta.AccountID = a.Id WHERE ta.TeamID=@Id and ta.TeamRoleID =1 for json path, without_array_wrapper)) as 'team.LeaderAccount',   " +
-      "(SELECT a.*,ta.TeamRoleID FROM [TeamAccount] as ta JOIN [Account] as a ON ta.AccountID = a.Id WHERE ta.TeamID=@Id and ta.TeamRoleID =2 ORDER BY a.IsActive DESC, a.Username  ASC for json path) as 'team.MemberAccounts', " +
-      // "json_query((SELECT l.Name,l.Address,p.PositionName,p.Block,p.Floor,p.Tile for json path, without_array_wrapper))  as 'team.Location'" +
-      // "json_query((SELECT DISTINCT l.Name,l.Address,p.PositionName,p.Block,p.Floor,p.Tile FROM [Team] as t JOIN [TeamLocation] as tl ON tl.TeamID = t.Id JOIN [Location] as l ON tl.LocationID = l.Id JOIN [Position] as p ON p.LocationID =l.Id for json path))  as 'team.Location' " +
-      "json_query((select l.* from [TeamLocation] as tl JOIN [Location] as l ON tl.LocationID =l.Id WHERE tl.TeamID =t.Id   for json path))  as 'team.Location' " +
-      "FROM [Team] as t	   " +
-      // "JOIN [TeamLocation] as tl ON tl.TeamID = t.Id " +
-      // "JOIN [Location] as l ON tl.LocationID = l.Id " +
-      // "JOIN [Position] as p ON p.LocationID =l.Id " +
-      "WHERE t.Id = @Id   " +
+      "SELECT t.id as 'team.Id', t.Name as 'team.Name', t.CreatedDate as 'team.CreatedDate',workorder.[Name] as [WorkOrderName], wos.[Name] as [WorkOrderStatusName],  " +
+      "json_query((SELECT a.*,ta.TeamRoleID FROM [TeamAccount] as ta JOIN [Account] as a ON ta.AccountID = a.Id WHERE ta.TeamID =@id and ta.TeamRoleID =1 for json path, without_array_wrapper)) as 'LeaderAccount', " +
+      "(SELECT a.*,ta.TeamRoleID FROM [TeamAccount] as ta JOIN [Account] as a ON ta.AccountID = a.Id WHERE ta.TeamID =@id and ta.TeamRoleID =2 ORDER BY a.IsActive DESC, a.Username  ASC for json path) as 'MemberAccounts', " +
+      "json_query((select l.* from [TeamLocation] as tl JOIN [Location] as l ON tl.LocationID =l.Id WHERE tl.TeamID =t.Id   for json path))  as 'LocationOfTeam', " +
+      "json_query((select e.Id, e.[Name], e.[Image], u.[Name] as [Unit], (select ei2.*, " +
+      "json_query((select wodr.*, es.[Name] as [ReturnedStatusName] " +
+      "from WorkOrderDetailReturn as wodr " +
+      "join EquipmentStatus as es on wodr.ReturnedStatusId = es.Id " +
+      "where wodr.WorkOrderDetailId = wod2.Id " +
+      "for json path, without_array_wrapper)) as [DetailReturn] " +
+      "from WorkOrder as wo2 join WorkOrderDetail as wod2 on wo2.Id = wod2.WorkOrderID " +
+      "join EquipmentItem as ei2 on wod2.EquipmentItemID = ei2.Id " +
+      "where wo2.Id = workorder.Id and ei2.EquipmentID = e.Id for json path) as [EquipmentItems] " +
+      "from Equipment as e join [Unit] as u on e.UnitID = u.Id " +
+      "where e.Id in (select distinct e.Id " +
+      "from Equipment as e join EquipmentItem as ei on e.Id = ei.EquipmentID " +
+      "where ei.id in (select wod.EquipmentItemID " +
+      "from WorkOrder as wo join WorkOrderDetail as wod on wo.Id = wod.WorkOrderID " +
+      "where wo.Id = workorder.Id)) " +
+      "for json path)) as [Equipment] " +
+      "FROM Team as t " +
+      "JOIN [TeamLocation] as tl ON tl.TeamID = t.Id " +
+      "JOIN [WorkOrder] as workorder ON workorder.TeamLocationID = tl.Id " +
+      "join [WorkOrderStatus] as wos on workorder.StatusID = wos.Id " +
+      "WHERE t.Id = @id " +
       "for json path, without_array_wrapper"
 
     )
