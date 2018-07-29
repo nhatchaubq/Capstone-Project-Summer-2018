6fb69fc3f01ed78be0bb0d7e741b0819286d4bf6
hautt <hautt1995@gmail.com>
2018-07-29 11:26:08 +0700

    aa

--- code/EMS/ems/src/App.vue
+++ code/EMS/ems/src/App.vue
@@ -24,13 +24,7 @@ import HeaderBar from "./components/HeaderBar/HeaderBar";
 import Server from "@/config/config";
 
 import Vue from "vue";
-import BootstrapVue from "bootstrap-vue/dist/bootstrap-vue.esm";
 
-// Import the styles directly. (Or you could add them via script tags.)
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-
-Vue.use(BootstrapVue);
 var VueTruncate = require("vue-truncate-filter");
 Vue.use(VueTruncate);
 
